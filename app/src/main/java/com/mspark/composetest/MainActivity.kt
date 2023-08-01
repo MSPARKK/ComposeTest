882a3066abe88f3cee2c65e198813030d2f1c104
MSPARKK <pms93009@gmail.com>
2023-08-01 13:24:04 +0900

    토스트 띄우는 버튼 추가

--- app/src/main/java/com/mspark/composetest/MainActivity.kt
+++ app/src/main/java/com/mspark/composetest/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.mspark.composetest
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.mspark.composetest.ui.theme.ComposeTestTheme
 
@@ -16,13 +22,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            MainApp()
+        }
+    }
+}
+
+@Composable
+fun MainApp() {
+    ComposeTestTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column {
+                    Greeting("Android Android Android Android")
+                    SetButton("버튼 버튼")
                 }
             }
         }
@@ -34,10 +52,19 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun SetButton(text: String) {
+    val context = LocalContext.current
+
+    Button(onClick = {
+        Toast.makeText(context, "버튼 클릭!", Toast.LENGTH_SHORT).show()
+    }) {
+        Text(text = text)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeTestTheme {
-        Greeting("Compose")
-    }
+    MainApp()
 }
