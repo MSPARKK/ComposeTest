bf3a8ec02936b2b7924331e9352ed3c865538b2a
MSPARKK <pms93009@gmail.com>
2023-08-02 13:25:19 +0900

    Text 이것저것 바꿔보기

--- app/src/main/java/com/mspark/composetest/MainActivity.kt
+++ app/src/main/java/com/mspark/composetest/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +18,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mspark.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,7 +60,21 @@ fun MainApp() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+//        Text(text = "Hello $name!")
+//        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+        Text(
+            text = "Hello $name!",
+//            style = MaterialTheme.typography.h6,
+            style = TextStyle(
+                fontSize = 20.sp,
+                color = Color.Yellow,
+                fontWeight = FontWeight.Bold
+            ),  // 커스텀 텍스트 스타일
+            modifier = Modifier.padding(32.dp)
+        )
+    }
+
 }
 
 @Composable
