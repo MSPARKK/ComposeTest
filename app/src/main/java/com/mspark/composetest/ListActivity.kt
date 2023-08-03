fe3820a7d049d867d1cd1646ef153805bab1c172
MSPARKK <pms93009@gmail.com>
2023-08-03 13:27:45 +0900

    이미지 border 추가, 이미지 clip 모양 테스트

--- app/src/main/java/com/mspark/composetest/ListActivity.kt
+++ app/src/main/java/com/mspark/composetest/ListActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -74,7 +77,7 @@ private fun Greeting(testData: TestData) {
             Box(
                 modifier = Modifier
                     .padding(start = 20.dp)
-                    .size(60.dp),
+                    .size(62.dp),
                 contentAlignment = Alignment.Center
             ) {
                 Image(
@@ -82,7 +85,16 @@ private fun Greeting(testData: TestData) {
                     contentDescription = "My image description",
                     modifier = Modifier
                         .fillMaxSize()
-                        .clip(CircleShape),
+                        .clip(CircleShape)
+//                        .clip(
+//                            CutCornerShape(
+//                                topStart = 4.dp,
+//                                topEnd = 8.dp,
+//                                bottomEnd = 16.dp,
+//                                bottomStart = 8.dp
+//                            )
+//                        )
+                        .border(1.dp, Color.Gray, CircleShape),
                     contentScale = ContentScale.Crop,
                 )
             }
