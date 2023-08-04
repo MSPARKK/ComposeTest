2369e28e157981701d90d2933fb5c6ed8b620e9d
MSPARKK <pms93009@gmail.com>
2023-08-04 15:44:39 +0900

    1. LazyColumn을 사용해서 스크롤 리스트로 변경 2. TestData에 detail를 추가하고 아이템 하단에 텍스트를 추가하여 expaned 에 따라 보여주고 숨기기 처리

--- app/src/main/java/com/mspark/composetest/ListActivity.kt
+++ app/src/main/java/com/mspark/composetest/ListActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -56,6 +61,24 @@ fun ListApp() {
         TestData("Lebron", "https://media.cnn.com/api/v1/images/stellar/prod/230523093708-01-lebron-james-052223.jpg?c=9x16"),
         TestData("Wembanyama", "https://s.hdnux.com/photos/01/33/70/11/24085095/3/1200x0.jpg"),
         TestData("Sochan", "https://images2.minutemediacdn.com/image/upload/c_crop,w_4656,h_2619,x_0,y_41/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/29/01gr2smda6b9877kawpt.jpg"),
+        TestData("Pop", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKq.WwqakWoQ/v2/1200x-1.jpg"),
+
+        TestData("Kobe","https://hips.hearstapps.com/hmg-prod/images/gettyimages-159141331-1580077058.jpg?crop=0.5446666666666666xw:1xh;center,top&resize=1200:*"),
+        TestData("Lebron", "https://media.cnn.com/api/v1/images/stellar/prod/230523093708-01-lebron-james-052223.jpg?c=9x16"),
+        TestData("Wembanyama", "https://s.hdnux.com/photos/01/33/70/11/24085095/3/1200x0.jpg"),
+        TestData("Sochan", "https://images2.minutemediacdn.com/image/upload/c_crop,w_4656,h_2619,x_0,y_41/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/29/01gr2smda6b9877kawpt.jpg"),
+        TestData("Pop", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKq.WwqakWoQ/v2/1200x-1.jpg"),
+
+        TestData("Kobe","https://hips.hearstapps.com/hmg-prod/images/gettyimages-159141331-1580077058.jpg?crop=0.5446666666666666xw:1xh;center,top&resize=1200:*"),
+        TestData("Lebron", "https://media.cnn.com/api/v1/images/stellar/prod/230523093708-01-lebron-james-052223.jpg?c=9x16"),
+        TestData("Wembanyama", "https://s.hdnux.com/photos/01/33/70/11/24085095/3/1200x0.jpg"),
+        TestData("Sochan", "https://images2.minutemediacdn.com/image/upload/c_crop,w_4656,h_2619,x_0,y_41/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/29/01gr2smda6b9877kawpt.jpg"),
+        TestData("Pop", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKq.WwqakWoQ/v2/1200x-1.jpg"),
+
+        TestData("Kobe","https://hips.hearstapps.com/hmg-prod/images/gettyimages-159141331-1580077058.jpg?crop=0.5446666666666666xw:1xh;center,top&resize=1200:*"),
+        TestData("Lebron", "https://media.cnn.com/api/v1/images/stellar/prod/230523093708-01-lebron-james-052223.jpg?c=9x16"),
+        TestData("Wembanyama", "https://s.hdnux.com/photos/01/33/70/11/24085095/3/1200x0.jpg"),
+        TestData("Sochan", "https://images2.minutemediacdn.com/image/upload/c_crop,w_4656,h_2619,x_0,y_41/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/images/GettyImages/mmsport/29/01gr2smda6b9877kawpt.jpg"),
         TestData("Pop", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKq.WwqakWoQ/v2/1200x-1.jpg")
     )
 
@@ -65,10 +88,10 @@ fun ListApp() {
             color = Color.White
         ) {
 
-            Column(
+            LazyColumn(
                 modifier = Modifier.padding(vertical = 10.dp)
             ) {
-                for (testData in testDataList) {
+                items(testDataList) { testData ->
                     Greeting(testData = testData)
                 }
             }
@@ -91,56 +114,60 @@ private fun Greeting(testData: TestData) {
             .clip(RoundedCornerShape(16.dp))
             .shadow(elevation = 8.dp, shape = RoundedCornerShape(16.dp)), // 그림자가 안생기는 거 같다?
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Box(
-                modifier = Modifier
-                    .padding(start = 20.dp)
-                    .size(62.dp)
-            ) {
-                Image(
-                    painter = rememberImagePainter(data = testData.imageUrl),
-                    contentDescription = "My image description",
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Box(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                        .border(1.dp, Color.Gray, CircleShape),
-                    contentScale = ContentScale.Crop,
-                )
-            }
-
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(24.dp)
-            ) {
-                Text(text = "Hello,", color = Color.White)
-                Text(text = testData.name, color = Color.White)
-            }
-
-
-            Box(
-                modifier = Modifier
-                    .padding(end = 10.dp)
-                    .size(60.dp)
-            ) {
-                val context = LocalContext.current
-
-                IconButton(onClick = {
-                    Toast.makeText(context, "아이콘 버튼 클릭!", Toast.LENGTH_SHORT).show()
-                    expanded.value = !expanded.value
-                }) {
-                    Icon(
-                        imageVector = if (expanded.value) Icons.Rounded.KeyboardArrowUp else Icons.Rounded.KeyboardArrowDown,
-                        contentDescription = null,
+                        .padding(start = 20.dp)
+                        .size(62.dp)
+                ) {
+                    Image(
+                        painter = rememberImagePainter(data = testData.imageUrl),
+                        contentDescription = "My image description",
                         modifier = Modifier
                             .fillMaxSize()
-                            .padding(14.dp)
+                            .clip(CircleShape)
+                            .border(1.dp, Color.Gray, CircleShape),
+                        contentScale = ContentScale.Crop,
                     )
                 }
+
+                Column(modifier = Modifier
+                    .weight(1f)
+                    .padding(24.dp)
+                ) {
+                    Text(text = "Hello,", color = Color.White)
+                    Text(text = testData.name, color = Color.White)
+                }
+
+
+                Box(
+                    modifier = Modifier
+                        .padding(end = 10.dp)
+                        .size(60.dp)
+                ) {
+                    IconButton(onClick = {
+                        expanded.value = !expanded.value
+                    }) {
+                        Icon(
+                            imageVector = if (expanded.value) Icons.Rounded.KeyboardArrowUp else Icons.Rounded.KeyboardArrowDown,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(14.dp)
+                        )
+                    }
+                }
+
             }
 
+            AnimatedVisibility(visible = expanded.value) {
+                Text(text = testData.detail)
+            }
         }
+
        
     }
 }
@@ -153,5 +180,13 @@ fun ListPreview() {
 
 data class TestData(
     val name: String,
-    val imageUrl: String
+    val imageUrl: String,
+    val detail: String = "어쩌구 저쩌구 블라블라 Jetpack Compose로\n" +
+            "더 빠르게 더 나은 Android 앱 빌드\n" +
+            "Jetpack Compose는 네이티브 UI를 빌드하기 위한 Android의 최신 권장 도구 키트입니다. Jetpack Compose는 Android에서의 UI 개발을 간소화하고 가속화합니다. 간단한 코드, 강력한 도구, 직관적인 Kotlin API를 사용하여 앱을 빠르고 생동감 있게 구현하세요.\n 직관적\n" +
+            "Compose는 선언적 API를 사용합니다. 즉, Compose가 나머지를 처리하므로 UI를 설명하기만 하면 됩니다. API는 직관적이므로 찾아서 사용하기가 쉽습니다. \"테마 레이어가 훨씬 더 직관적이고 읽기 쉽습니다. 멀티 레이어 테마 오버레이를 통해 속성을 정의하고 할당하는 여러 XML 파일로 확장해야 했을 일을 단일한 Kotlin 파일 내에서 달성할 수 있었습니다.” (Twitter)\n" +
+            "\n" +
+            "Compose를 사용하면 특정 활동이나 프래그먼트에 종속되지 않는 작은 스테이트리스(Stateless) 구성요소를 빌드합니다. 이를 통해 재사용하고 테스트하기가 쉬워집니다. \"사용 및 유지관리가 쉽고 구현/확장/맞춤설정이 직관적인 스테이트리스 UI 구성요소의 새로운 세트를 제공하겠다는 목표를 세웠습니다. Compose는 이를 위한 좋은 해답이었습니다.” (Twitter)\n" +
+            "\n" +
+            "Compose에서는 상태가 명시적이며 컴포저블로 전달됩니다. 이렇게 하면 상태에 관한 정보 출처가 하나이므로 캡슐화되고 분리됩니다. 그런 다음 앱 상태가 변경되면 UI가 자동으로 업데이트됩니다. “추론하는 동안 기억해야 하는 사항이 줄어들고 통제를 벗어나거나 제대로 이해하지 못하는 행동도 적어집니다.” (Cuvva)"
 )
